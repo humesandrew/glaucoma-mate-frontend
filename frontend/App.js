e8e0d5e7bcab492930b0a64dc7dcec92ae2eea6f
Andy Humes <andrew.humes@du.edu>
2023-08-05 13:22:05 -0600

    addhomeasroute

--- frontend/App.js
+++ frontend/App.js
@@ -1,40 +1,25 @@
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import Header from './components/Header';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import Header from "./components/Header";
+import Home from "./screens/home";
 
 export default function App() {
-  const [email, setEmail] = useState('Email');
+  const [email, setEmail] = useState("Email");
   const [password, setPassword] = useState("Password");
 
   return (
-    <TouchableWithoutFeedback onPress={() => {
-      Keyboard.dismiss();
-    }}>
-    <View style={styles.container}>
-     <Header />
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello world!</Text>
-        <Text style={styles.subtitle}>This is glaucoma-mate.</Text>
-        <View style={styles.formContainer}>
-          <Text>Email</Text>
-        <TextInput 
-        style={styles.input}
-        onChangeText={(val) => setEmail(val)} />
-        <Text>Password</Text>
-        <TextInput 
-        style={styles.input}
-        onChangeText={(val) => setPassword(val)} />
-
-      
-        <Text>Email entered: {email}</Text>
-        <Text>Password entered: {password}</Text>
-        </View>
-      
-      </View>
-      <StatusBar style="auto" />
-    </View>
-    </TouchableWithoutFeedback>
+  
+      <Home />
+   
   );
 }
 
@@ -42,30 +27,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 0,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
   },
   formContainer: {
-    alignItems: 'center', 
-    marginTop: 20
+    alignItems: "center",
+    marginTop: 20,
   },
   main: {
     flex: 1,
-    justifyContent: 'center',
-  }, 
+    justifyContent: "center",
+  },
   title: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 50,
   },
   subtitle: {
-    fontStyle: 'italic'
+    fontStyle: "italic",
   },
   input: {
     borderWidth: 1,
-    borderColor: '#777',
+    borderColor: "#777",
     padding: 8,
     margin: 10,
-    width: 300
-
-  }
+    width: 300,
+  },
 });
