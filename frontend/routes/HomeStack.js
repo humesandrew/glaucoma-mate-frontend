9ff721bdc9e4c8e2beb0299f2b16440ac850b620
Andy Humes <andrew.humes@du.edu>
2024-04-15 21:40:11 -0600

    loginwithfirebasenow

--- frontend/routes/HomeStack.js
+++ frontend/routes/HomeStack.js
@@ -1,47 +1,57 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useAuthContext } from "../hooks/useAuthContext.js";
-import { auth } from "../firebase";
-import { useNavigation } from "@react-navigation/native";
 import Auth from "../screens/Auth.js";
 import Doses from "../screens/Doses.js";
 import Manage from "../screens/Manage.js";
 import Signup from "../screens/Signup.js";
+import { auth } from "../firebase";
+import { getIdToken } from "firebase/auth";
 
 const Stack = createNativeStackNavigator();
 
 const HomeStack = () => {
-  // Access the user context
-  const { user, dispatch } = useAuthContext();
-  const navigation = useNavigation();
-
-  // State to track if the user is logged in
-  const [isLoggedIn, setIsLoggedIn] = useState(!!user);
+  const { dispatch } = useAuthContext();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(async (firebaseUser) => {
-      if (!user && firebaseUser) {
-        // Update AuthContext with the Firebase user if it's empty
-        dispatch({ type: "LOGIN", payload: firebaseUser });
-        setIsLoggedIn(true);
-      } else if (!firebaseUser && isLoggedIn) {
-        // Reset isLoggedIn when the user logs out
-        setIsLoggedIn(false);
-        // Navigate to the 'Signin' screen when the user logs out
-        navigation.navigate("Login");
+      if (firebaseUser) {
+        try {
+          const token = await getIdToken(firebaseUser);
+          console.log("HomeStack token:", token);
+          const response = await fetch('https://glaucoma-mate-backend.onrender.com/api/user/login', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`,
+            }
+          });
+
+          if (!response.ok) {
+            const errData = await response.json();
+            throw new Error("Failed to synchronize with backend: " + errData.error);
+          }
+
+          const userData = await response.json();
+          console.log("Fetched userData:", userData);
+          dispatch({ type: 'LOGIN', payload: { ...userData, authToken: token } });
+        } catch (error) {
+          console.error('Synchronization error:', error);
+        }
+      } else {
+        dispatch({ type: "LOGOUT" });
       }
     });
 
-    // Clean up the listener on component unmount
     return () => unsubscribe();
-  }, [dispatch, user, isLoggedIn, navigation]);
+  }, [dispatch]);
 
   return (
     <Stack.Navigator>
       <Stack.Screen name="Login" component={Auth} />
       <Stack.Screen name="Doses" component={Doses} options={{ headerShown: true, title: "Track your doses" }} />
-      <Stack.Screen name="Manage" component={Manage} options={{ headerShown: true, title: "Doses" }} />
-      <Stack.Screen name="Signup" component={Signup} options={{ headerShown: true, title: "Login" }} />
+      <Stack.Screen name="Manage" component={Manage} options={{ headerShown: true, title: "Manage Medications" }} />
+      <Stack.Screen name="Signup" component={Signup} options={{ headerShown: true, title: "Signup" }} />
     </Stack.Navigator>
   );
 };
