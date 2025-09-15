724807f0847fd96ae076d0788e6f9774d4ede9e3
Andy Humes <andrew.humes@du.edu>
2025-09-15 16:55:13 -0600

    retryeassecrets

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "glaucoma-mate-frontend",
     slug: "glaucoma-mate-frontend",
-    version: "1.0.4",
+    version: "1.0.5",
     orientation: "portrait",
     icon: "./assets/glaucomabuddylogo.png",
     userInterfaceStyle: "light",
