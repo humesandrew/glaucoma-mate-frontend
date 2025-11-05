117a226100487313172331ca85f9604fbe7cdae1
Andy Humes <andrew.humes@du.edu>
2025-11-04 21:00:48 -0700

    bumpvers

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "glaucoma-mate-frontend",
     slug: "glaucoma-mate-frontend",
-    version: "1.0.13",
+    version: "1.0.14",
     orientation: "portrait",
     icon: "./assets/glaucomabuddylogo.png",
     userInterfaceStyle: "light",
