3e243bc5d4b135a68a5a6f2b1a9bf071bd5948ab
Andy Humes <andrew.humes@du.edu>
2026-03-03 21:03:37 -0700

    bumpbuildtorebuild

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "glaucoma-mate-frontend",
     slug: "glaucoma-mate-frontend",
-    version: "1.0.14",
+    version: "1.0.15",
     orientation: "portrait",
     icon: "./assets/glaucomabuddylogo.png",
     userInterfaceStyle: "light",
